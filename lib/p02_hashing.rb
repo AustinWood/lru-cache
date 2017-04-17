b56b57323c5186c58e4c648972b9d46f76caff13
Austin Wood <austin@austinkwood.com>
2017-04-17 08:08:19 -0700

    Write hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,18 +4,28 @@ end
 
 class Array
   def hash
+    length = self.length
+    hashed_arr = self.map{ |el| el.hash }
+    new_arr = []
+    hashed_arr.each_with_index do |el, i|
+      new_arr << el * (i + 1)
+    end
+    new_arr << 123
+    length + new_arr.inject(:+) + new_arr.inject(:*)
   end
 end
 
 class String
   def hash
+    ord_arr = self.chars.map { |char| char.ord }
+    ord_arr.hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    keys = self.keys
+    vals = self.values
+    keys.sort.hash + vals.sort.hash
   end
 end
