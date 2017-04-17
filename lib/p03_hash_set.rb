f3f3f6fc752a5a1e480b4caf7a1fcd8c70efc5e2
Austin Wood <austin@austinkwood.com>
2017-04-17 08:28:00 -0700

    Write HashSet

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,24 +9,41 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      resize! if @count == num_buckets
+      @store[key.hash % num_buckets] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    @store[key.hash % num_buckets].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @store[key.hash % num_buckets].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
+  # def [](num)
+  #   # optional but useful; return the bucket corresponding to `num`
+  # end
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store[el % (num_buckets * 2)] << el
+      end
+    end
+    @store = new_store
   end
 end
